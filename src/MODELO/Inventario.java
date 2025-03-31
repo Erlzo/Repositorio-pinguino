77c2f4122f734e936e4ddf3bad9ccf16f95ed77b
Erlzo <ericromerogasol53@gmail.com>
2025-03-31 20:17:14 +0200

    Constructor clase inventario

--- src/MODELO/Inventario.java
+++ src/MODELO/Inventario.java
@@ -2,4 +2,40 @@ package MODELO;
 
 public class Inventario {
 
+	int dados;
+	int peces;
+	int bolasNieve;
+
+	public Inventario(int dados, int peces, int bolasNieve) {
+		this.dados = dados;
+		this.peces = peces;
+		this.bolasNieve = bolasNieve;
+	}
+
+	public int getDados() {
+		return dados;
+	}
+
+	public void setDados(int dados) {
+		this.dados = dados;
+	}
+
+	public int getPeces() {
+		return peces;
+	}
+
+	public void setPeces(int peces) {
+		this.peces = peces;
+	}
+
+	public int getBolasNieve() {
+		return bolasNieve;
+	}
+
+	public void setBolasNieve(int bolasNieve) {
+		this.bolasNieve = bolasNieve;
+	}
+
+
+
 }
