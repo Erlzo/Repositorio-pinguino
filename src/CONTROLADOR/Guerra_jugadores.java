e044768b57e35340d65b70c9965d95bee3b8dc01
Cyber-Alexis <afrancesretamero50@gmail.com>
2025-03-31 16:44:28 +0200

    Constructor Guerra Jugadores
    Creamos el constructor con las variables escogidas y declaramos los getters y settes.

--- src/CONTROLADOR/Guerra_jugadores.java
+++ src/CONTROLADOR/Guerra_jugadores.java
@@ -1,5 +1,45 @@
 package CONTROLADOR;
 
 public class Guerra_jugadores {
+	
+String jugadores;
+int bolas = 0;
+int casillas = 0;
+	
+
+public Guerra_jugadores(String jugadores, int bolas, int casillas) {
+	
+	this.jugadores = jugadores;
+	this.bolas = bolas;
+	this.casillas = casillas;
+
+}
+	
+//getters y setters
+public void setJugadores(String jugadores){
+	this.jugadores = jugadores;
+}
+
+public void setBolas(int bolas){
+	this.bolas = bolas;
+}
+
+public void setCasillas(int casillas){
+	this.casillas = casillas;
+}
+	
+public String getJugadores() {
+	return jugadores;
+	
+}
+public int getBolas() {
+	return bolas;
+	
+}
+public int getCasillas() {
+	return casillas;
+	
+}
+
 
 }
