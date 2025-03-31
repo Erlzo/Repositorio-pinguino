c942c9819b3793100fd341b36632a647d0a825ef
AlexFR0 <afrancesretamero50@gmail.com>
2025-03-31 21:11:53 +0200

    Constructor CPU-FOCA
    Creamos el constructor con las variables escogidas y declaramos los getters y settes.

--- src/MODELO/Cpu_Foca.java
+++ src/MODELO/Cpu_Foca.java
@@ -1,5 +1,44 @@
 package MODELO;
 
 public class Cpu_Foca {
+	
+	 public int posicion;
+	 public int inventario;
+	 public int ultimoAgujero;
+	
+	
+	 public Cpu_Foca(int  posicion , int inventario, int ultimoAgujero) {
+		 
+	        this.posicion = posicion;
+	        this.inventario = inventario;
+	        this.ultimoAgujero = 0; 
+	    }
+	    
+	//getters y setters
+	
+	    
+	    public int getPosicion() {
+	        return posicion;
+	    }
+	    
+	    public void setPosicion(int nuevaPosicion) {
+	        this.posicion = nuevaPosicion;
+	    }
+	    
+	    public int getInventario() {
+	        return inventario;
+	    }
+	    
+	    public void setInventario(int nuevoInventario) {
+	        this.inventario = nuevoInventario;
+	    }
+	    
+	    public int getUltimoAgujero() {
+	        return ultimoAgujero;
+	    }
+	    
+	    public void setUltimoAgujero(int agujero) {
+	        this.ultimoAgujero = agujero;
+	    }
 
 }
