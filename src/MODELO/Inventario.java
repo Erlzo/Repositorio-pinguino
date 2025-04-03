30f124979c194d77df8b11138a825375e515d748
Rocky0524 <kenbalboa05@gmail.com>
2025-04-03 16:50:41 +0200

    Avanze clase inventario
    agrego varias funciones a la clase inventario

--- src/MODELO/Inventario.java
+++ src/MODELO/Inventario.java
@@ -1,23 +1,35 @@
 package MODELO;
 
+import java.util.Scanner;
+
 public class Inventario {
+	Scanner scanner = new Scanner(System.in);
+	int dadoRapido = 1;
+	int dadoLentos = 1;
+	int peces = 1;
+	int bolasNieve = 2;
 
-	int dados;
-	int peces;
-	int bolasNieve;
-
-	public Inventario(int dados, int peces, int bolasNieve) {
-		this.dados = dados;
+	public Inventario(int dadoRapido, int dadoLentos, int peces, int bolasNieve) {
+		this.dadoRapido = dadoRapido;
+		this.dadoLentos = dadoLentos;
 		this.peces = peces;
 		this.bolasNieve = bolasNieve;
 	}
 
-	public int getDados() {
-		return dados;
+	public int getdadoRapido() {
+		return dadoRapido;
 	}
 
-	public void setDados(int dados) {
-		this.dados = dados;
+	public void setdadoLentos(int dadoLentos) {
+		this.dadoLentos = dadoLentos;
+	}
+
+	public int getdadoLentos() {
+		return dadoLentos;
+	}
+
+	public void setdadoRapido(int dadoRapido) {
+		this.dadoRapido = dadoRapido;
 	}
 
 	public int getPeces() {
@@ -36,6 +48,27 @@ public class Inventario {
 		this.bolasNieve = bolasNieve;
 	}
 
+	public void mostrarInventario(String jugador, int dadoRapido, int dadoLentos, int peces, int bolasNieve) {
 
+		System.out.println("Este es tu inventario: ");
+		System.out.println("1-Dados rapidos: " + this.dadoRapido);
+		System.out.println("2-Dados lentos: " + this.dadoLentos);
+		System.out.println("3-Peces: " + this.peces);
+		System.out.println("4-Bolas de nieve: " + this.bolasNieve);
+
+	}
+
+	public int eleccionInventario(String jugador, int dadoRapido, int dadoLentos) {
+
+		System.out.println("Este es tu inventario: ");
+		System.out.println("1-Dado normal");
+		System.out.println("2-Dados rapidos: " + this.dadoRapido);
+		System.out.println("3-Dados lentos: " + this.dadoLentos);
+		System.out.println("4-Atras");
+		System.out.println("Que quieres hacer? ");
+		int eleccion = scanner.nextInt();
+		return eleccion;
+
+	}
 
 }
