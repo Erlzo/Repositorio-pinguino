8337648e08a0c71ecc04ec97d09140b409f5432a
Alexiito <afrancesretamero50@gmail.com>
2025-04-03 16:59:13 +0200

    Clase Eventos
    Creamos un swtich-case  con un random para escoger uno de los eventos aleatoriamente.
    
    Creamos 2 metodos para los tipos de dados (lento y rapido).

--- src/CONTROLADOR/Eventos.java
+++ src/CONTROLADOR/Eventos.java
@@ -1,9 +1,40 @@
 package CONTROLADOR;
 
+
 public class Eventos {
 	
+	Random random = new Random();
+    int evento = random.nextInt(4); 
 
+    switch (evento) {
+        case 0:
+            return ObtenerPescado();
+        case 1:
+            return obtenerBolaNieve();
+        case 2:
+            return obetnerDadoRapido();
+        case 3:
+            return obtenerDadoLento();
+        default:
+            return "🎟 No has elegido ningun evento 🎟";
+    }
+    
+    public static String obetnerDadoRapido() {
+            int casillas = new Random().nextInt(6) + 5; 
+            return "Has conseguido el dado rapido --> " + casillas + " casillas ";
+        }
+        return "No has conseguido el dado rapido 🎲";
+    }
+
+  
+    public static String obtenerDadoLento() {
+        int casillas = new Random().nextInt(3) + 1; 
+        return "Has conseguido el dado lento --> " + casillas ;
+    }
+}
 	
+    
+}
 	
 	
 	
